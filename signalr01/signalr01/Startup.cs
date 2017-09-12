14af11046e312aef1fe334dbe6fb2b1e534ece67
lapadol <lapadol@builk.com>
2017-09-12 13:39:15 +0700

    import signalr to project by nupkg file
    How to import nupkg [[> Tool => NuGet Package Manager => Package Manager Setting, In Package Source Click + for brows source directory folder have a nupkg file

--- signalr01/signalr01/Startup.cs
+++ signalr01/signalr01/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SignalR;
 
 namespace signalr01
 {
@@ -22,6 +23,11 @@ namespace signalr01
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR(options =>
+            {
+                //options.Hubs.EnableDetailedErrors = true;
+            });
+
             services.AddMvc();
         }
 
@@ -40,6 +46,10 @@ namespace signalr01
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            //app.UseSignalR();
 
             app.UseStaticFiles();
 
